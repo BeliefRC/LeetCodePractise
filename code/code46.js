cac8429ab3b5c6944d1e7dd98303bbf785c03114
BeliefRC <beliefrc@outlook.com>
2020-08-27 23:15:04 +0800

    fix:46

--- code/code46.js
+++ code/code46.js
@@ -4,13 +4,16 @@
  */
 var permute = function (nums) {
   const result = []
+
   function fn (prev, next) {
     if (prev.length === nums.length) {
       result.push(prev)
     } else {
-      const temp = [].concat(next)
-      const item = temp.pop()
-      fn(prev.concat(item), temp)
+      for (let i = 0; i < next.length; i++) {
+        const temp = [].concat(next)
+        const item = temp.splice(i, 1)
+        fn(prev.concat(item), temp)
+      }
     }
   }
 
